3a7981523a323495c751f2b50cf6add8dabf229a
Matthew Waring <matthew.waring@financialedge.co.uk>
2015-04-13 10:01:01 +0100

    Added .aspx naming strategy.

--- PageJaunesResto.WebAPI.Connectivity.Framework/RequestCommands/NamingStrategies/TraditionalStyleNamingStrategy.cs
+++ PageJaunesResto.WebAPI.Connectivity.Framework/RequestCommands/NamingStrategies/TraditionalStyleNamingStrategy.cs
@@ -17,4 +17,13 @@ namespace PageJaunesResto.WebAPI.Connectivity.Framework.RequestCommands.NamingSt
             return name + "/" + methodName;
         }
     }
+
+    public class AspxStyleNamingStrategy : IRequestBaseNameStrategy
+    {
+        public string GetBaseName(string className, string methodName)
+        {
+            var name = className.EndsWith("Controller") ? className.Replace("Controller", "") : className;
+            return name + "/" + methodName + ".aspx";
+        }
+    }
 }
